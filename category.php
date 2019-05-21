3ca5691bcb3c1a225443cc7c96ef76991bb16ca9
Tim King <tim.king@healthalliance.org>
2019-05-21 14:35:59 -0500

    newsroom tweaks 2

--- category.php
+++ category.php
@@ -1,11 +1,15 @@
 <!-- CATEGORY -->
 <?php get_header(); ?>
 
-	<section class="row">
-		<div class="small-12 large-9 columns">
-			<h1><?php _e( 'Category: ', 'html5blank' ); single_cat_title(); ?></h1>
+	<section class="primary">
+		<div class="single-page">
+			<h1 class="title"><?php _e( '', 'html5blank' ); single_cat_title(); ?></h1>
 		
-			
+			<?php #get_template_part('loop'); ?>
+
+
+					<?php if ( $wp_query->have_posts() ) : ?>
+					<?php while ( $wp_query->have_posts() ) : $wp_query->the_post(); ?>
 					<!-- entry -->
 					<div class="content" id="post-<?php the_ID(); ?>">
 						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
@@ -16,12 +20,29 @@
 						?>
 						<p><a href="<?php the_permalink(); ?>">View Full Post</a></p>
 					</div>
-		
-			<?php get_template_part('pagination'); ?>
+					
+					<?php endwhile; ?>
+					<?php html5wp_pagination(); ?>
+					<?php else: ?>
+					
+					<!-- article -->
+					<article>
+						<p>
+							<?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?>
+						</p>
+					</article>
+					<!-- /article -->
+					<?php endif; ?>
+
+
 		</div>
-		<div class="small-12 large-3 columns">
+<!-- end primary content -->
+
+<!-- START SIDEBAR -->
 			<?php get_sidebar(); ?>
+<!-- end SIDEBAR -->
+			<div class="clear"></div>
 		</div>
-	</section>
+<!-- end wrapper main -->
 
 <?php get_footer(); ?>
