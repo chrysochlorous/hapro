ab178b99aac31c560dd2d550a2b5e04ddcdc7480
Tim King <tim.king@healthalliance.org>
2019-05-21 13:54:54 -0500

    clean up mailchimp

--- template-medicare-flashes.php
+++ template-medicare-flashes.php
@@ -7,23 +7,31 @@
 				<div class="single-page">
 					<h1 class="title"><?php the_title(); ?></h1>
 					<!-- <p class="page-description">Insert description here</p> -->
-				
+
+					<?php
+						//$current_site = get_blog_details();
+						//print_r($current_site);
+						if(strpos($_SERVER[HTTP_HOST], "group.healthalliance.org") !== false){
+							$mailchimp_id = "2f0333c675";
+						} else if(strpos($_SERVER[HTTP_HOST], "broker.healthalliance.org") !== false){
+							$mailchimp_id = "1a18ba1590";
+						}
+					?>			
 					<!-- Begin MailChimp Signup Form -->
 						<div id="mc_embed_signup" class="panel text-center">
-							<form action="//healthalliance.us12.list-manage.com/subscribe/post?u=6a55a9ae0c586bdd1a6a6ed4b&amp;id=401e149292" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<form action="//healthalliance.us12.list-manage.com/subscribe/post?u=6a55a9ae0c586bdd1a6a6ed4b&amp;id=<?php echo $mailchimp_id; ?>" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 								<div id="mc_embed_signup_scroll">
-										<p for="mce-EMAIL">Stay in the loop,<br>get emails of every new post!<br><br>
-										<input type="email" value="" name="EMAIL" class="email input-group-field text-center" id="mce-EMAIL" placeholder="email address" required>
+										<p for="mce-EMAIL">Stay in the loop, <a href="/feed/?post_type=flash">subscribe to RSS</a> or sign up for emails of every new post!<br>
+										<input type="email" value="" name="EMAIL" class="email input-group-field text-center" id="mce-EMAIL" placeholder="Your email address" required>
 										<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button expanded" style="padding:0.8rem 1em;"></p>
 									<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 									<div style="position: absolute; left: -5000px;" aria-hidden="true">
-										<input type="text" name="b_6a55a9ae0c586bdd1a6a6ed4b_401e149292" tabindex="-1" value="">
+										<input type="text" name="b_6a55a9ae0c586bdd1a6a6ed4b_<?php echo $mailchimp_id; ?>" tabindex="-1" value="">
 									</div>
 								</div>
 							</form>
 						</div>
 						<!--End mc_embed_signup-->
-						<p style="margin:-3rem 0 0" class="text-center"><small><a href="/feed/?post_type=flash">Subscribe to RSS</a></small></p>
 
 
 
@@ -58,15 +66,7 @@
 
 
 
-		<?php
-			//$current_site = get_blog_details();
-			//print_r($current_site);
-			if(strpos($_SERVER[HTTP_HOST], "group.healthalliance.org") !== false){
-				$mailchimp_id = "2f0333c675";
-			} else if(strpos($_SERVER[HTTP_HOST], "broker.healthalliance.org") !== false){
-				$mailchimp_id = "1a18ba1590";
-			}
-		?>
+
 
 
 
@@ -78,30 +78,6 @@
 
 <!-- START SIDEBAR -->
 			<?php get_sidebar(); ?>
-
-			<div class="box">
-                <div class="inner-container">
-					<div id="mailchimpit">
-					<!-- Begin MailChimp Signup Form -->
-						<div id="mc_embed_signup" class="panel text-center">
-							<form action="//healthalliance.us12.list-manage.com/subscribe/post?u=6a55a9ae0c586bdd1a6a6ed4b&amp;id=401e149292" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-								<div id="mc_embed_signup_scroll">
-										<p for="mce-EMAIL">Stay in the loop,<br>get emails of every new post!<br><br>
-										<input type="email" value="" name="EMAIL" class="email input-group-field text-center" id="mce-EMAIL" placeholder="email address" required>
-										<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button expanded" style="padding:0.8rem 1em;"></p>
-									<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-									<div style="position: absolute; left: -5000px;" aria-hidden="true">
-										<input type="text" name="b_6a55a9ae0c586bdd1a6a6ed4b_401e149292" tabindex="-1" value="">
-									</div>
-								</div>
-							</form>
-						</div>
-						<!--End mc_embed_signup-->
-						<p style="margin:-3rem 0 0" class="text-center"><small><a href="/feed/?post_type=flash">Subscribe to RSS</a></small></p>
-					</div>
-                </div>
-            </div>
-
 <!-- end SIDEBAR -->
 			<div class="clear"></div>
 		</div>
