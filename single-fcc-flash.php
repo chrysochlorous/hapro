2bbb17632c2b60d55a19bff8deb722836d48cc36
Tim King <Tim.King@healthalliance.org>
2022-03-04 15:00:18 -0600

    Update single-fcc-flash.php

--- single-fcc-flash.php
+++ single-fcc-flash.php
@@ -1,5 +1,5 @@
 <?php
-if(strpos($_SERVER[REQUEST_URI],'brokers') !== false){
+if(strpos($_SERVER[REQUEST_URI],'broker') !== false){
 	header("Location: https://firstcarolinacare.com/brokers/commercial-flashes");
 } else {
 	header("Location: https://firstcarolinacare.com/employers/flashes");
